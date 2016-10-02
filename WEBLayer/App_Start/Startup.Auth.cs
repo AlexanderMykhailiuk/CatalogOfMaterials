43bc4cd10aeb3831c5bf0b2199aa5d8000ed00cf
AlexanderMykhailiuk <sashahuan@gmail.com>
2016-10-02 03:53:56 +0300

    WEBLayer Upload p.1

---
+++ WEBLayer/App_Start/Startup.Auth.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Owin;
+using Owin;
+using Microsoft.Owin.Security.Cookies;
+using Microsoft.AspNet.Identity;
+using BusinessLogicLayer.Services;
+using BusinessLogicLayer.Interfaces;
+using System.Web.Configuration;
+
+[assembly: OwinStartup(typeof(WEBLayer.Startup))]
+
+namespace WEBLayer
+{
+    public partial class Startup
+    {
+        public void ConfigureAuth(IAppBuilder app)
+        {            
+            app.CreatePerOwinContext<IUserService>(CreateUserService);
+            app.UseCookieAuthentication(new CookieAuthenticationOptions
+            {
+                AuthenticationType = DefaultAuthenticationTypes.ApplicationCookie,
+                LoginPath = new PathString("/Account/Login"),
+            });
+        }
+
+        IServiceCreator serviceCreator = new ServiceCreator();
+
+        private IUserService CreateUserService()
+        {
+            string connection = WebConfigurationManager.ConnectionStrings["CatalogConnectionString"].ConnectionString;
+            return serviceCreator.CreateUserService(connection);
+        }
+    }
+}
